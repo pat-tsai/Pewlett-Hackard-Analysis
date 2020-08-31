06cdd224a62d92482ac748523bf6972ad76216ab
pat-tsai <67960210+pat-tsai@users.noreply.github.com>
2020-08-30 19:22:29 -0700

    challenge update

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,51 @@
+
+-- Finding retirees from each department
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement
+FROM employees as e
+INNER JOIN titles as t
+	ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (r.emp_no) r.emp_no,
+	r.first_name,
+	r.last_name,
+	r.title
+INTO unique_retirement
+FROM retirement as r
+ORDER BY r.emp_no ASC, r.to_date DESC;
+
+
+-- Retries number of employees about to retire
+SELECT COUNT (DISTINCT t.emp_no), t.title
+INTO retiring_count
+FROM unique_retirement AS t
+GROUP BY t.title
+ORDER BY COUNT (t.title) DESC;
+
+
+-- Selecting current employees eligible for mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	d.from_date,
+	d.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN dept_emp as d
+	ON e.emp_no = d.emp_no
+LEFT JOIN titles as t
+	ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND d.to_date = '9999-01-01'
+ORDER BY e.emp_no ASC;
--- Queries/queries.sql
+++ Queries/queries.sql
@@ -170,3 +170,103 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+
+-- Creating inner join
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Employee info list
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+	 
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+	
+-- Select department retirees 
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+-- INTO dept_info
+d.dept_name
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON ();
+
+
+
+-- Finding retirees from each department
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement
+FROM employees as e
+INNER JOIN titles as t
+	ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (r.emp_no) r.emp_no,
+	r.first_name,
+	r.last_name,
+	r.title
+INTO unique_retirement
+FROM retirement as r
+ORDER BY r.emp_no ASC, r.to_date DESC;
+
+
+-- Retries number of employees about to retire
+SELECT COUNT (DISTINCT t.emp_no), t.title
+INTO retiring_count
+FROM unique_retirement AS t
+GROUP BY t.title
+ORDER BY COUNT (t.title) DESC;
+
+SELECT * FROM retirement_info;
